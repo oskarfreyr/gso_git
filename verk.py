d218eadbb241bcdd3b64fd80c6e4c8ff8db5e83c
Óskar Freyr Jóhannsson <oskarjo274@nemi.tskoli.is>
2017-01-25 16:29:42 +0000

    forritið skrifar þrjár línur af eigin vali

--- verk.py
+++ verk.py
@@ -2,5 +2,16 @@
 #25.01.17
 
 skra = input("sláðu inn nafn á skrá")
-namskra = skra + ".txt"
-SkrainMin = open(namskra, 'w+')
+nafnskra = skra + ".txt"
+SkrainMin = open(nafnskra, 'w+')
+
+SkrainMin.write("Fyrsta Línan Í skránni")
+SkrainMin.close()
+
+lina1 = input("Skráðu inn línu eitt")
+lina2 = input("Skráðu inn línu tvö")
+lina3 = input("Skráðu inn línu þrjú")
+SkrainMin = open(nafnskra, 'a')
+SkrainMin.write(lina1 + '\n')
+SkrainMin.write(lina2 + '\n')
+SkrainMin.write(lina3 + '\n')
