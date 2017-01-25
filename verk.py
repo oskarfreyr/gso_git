4548372021f50d078088dfc969214972dc84667b
Óskar Freyr Jóhannsson <oskarjo274@nemi.tskoli.is>
2017-01-25 16:52:18 +0000

    Núna les forritið skjalið sem þú bjóst til

--- verk.py
+++ verk.py
@@ -5,7 +5,7 @@ skra = input("sláðu inn nafn á skrá")
 nafnskra = skra + ".txt"
 SkrainMin = open(nafnskra, 'w+')
 
-SkrainMin.write("Fyrsta Línan Í skránni")
+SkrainMin.write("Fyrsta Línan Í skránni"'\n')
 SkrainMin.close()
 
 lina1 = input("Skráðu inn línu eitt")
@@ -15,3 +15,7 @@ SkrainMin = open(nafnskra, 'a')
 SkrainMin.write(lina1 + '\n')
 SkrainMin.write(lina2 + '\n')
 SkrainMin.write(lina3 + '\n')
+SkrainMin.close()
+SkrainMin = open(nafnskra, 'r')
+print("")
+print(SkrainMin.read())
